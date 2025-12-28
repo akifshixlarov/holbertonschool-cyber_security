fe458a3f9042d242404cc859356727141de734c0
Akif <akifshixlarov@gmail.com>
2025-12-28 22:50:50 +0400

    done

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo whois "$1" | awk -F: '/(Registrant|Admin|Tech) (Name|Organization|Street|City|State|Postal Code|Country|Phone|Phone Ext|Fax|Fax Ext|Email)/ { gsub(/^[ \t]+/, "", $2); if ($1 ~ /Street/) print $1 "," $2 " "; else if ($1 ~ /Phone Ext|Fax Ext/) print $1 ":," $2; else print $1 "," $2 }' > "$1.csv"
+whois "$1" | awk -F': *' '/^(Registrant|Admin|Tech)/ && !/^Registry/ {f=$1; v=$2; if(f~/(Phone Ext|Fax Ext)$/) f=f":"; print f ", " v}' >"$1.csv"
